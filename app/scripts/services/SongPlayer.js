d566a8eb336aac4fdf6551fccf4da0cc6e1444e4
Ryan Munsch <rmunsch9632@gmail.com>
2017-11-15 16:08:44 -0500

    assignment 7 work completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,18 +29,38 @@
       currentSong = song;
     };
 
+
+    /*
+    * @function playSong
+    * @desc Plays audio file currently stored in currentBuzzObject and sets song.playing state value to true
+    * @param {Object} song
+    */
+    var playSong = function(song) {
+        currentBuzzObject.play();
+        song.playing = true;
+    };
+
+    /*
+    * @function SongPlayer.song
+    * @desc If currentSong is not equal to song it calls setSong and playSong, and if currentSong is equal to song AND the currentBuzzObject is paused, it calls playSong
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
           if (currentBuzzObject.isPaused()) {
-            currentBuzzObject.play();
+            playSong(song);
           }
         }
     };
 
+    /*
+    * @function SongPlayer.pause
+    * @desc Pauses audio file and sets song.playing to false
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
